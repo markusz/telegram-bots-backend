5fb38f968f4cc2e7f878cbdc85adf7172d1ec309
Markus Ziller <markus.ziller@netlight.com>
2016-09-17 17:03:55 +0200

    Simplifing code in telegram-message.js Class

--- models/telegram-message.js
+++ models/telegram-message.js
@@ -1,27 +1,23 @@
 'use strict';
 
-const util = require('util');
+module.exports.inject = () => class TelegramMessage {
+  constructor(message) {
+    this.message = message;
+  }
 
-module.exports.inject = () => {
-  return class TelegramMessage {
-    constructor(message) {
-      this.message = message;
-    }
+  static getInstance(message) {
+    return new TelegramMessage(message);
+  }
 
-    static getInstance(message) {
-      return new TelegramMessage(message);
-    }
+  getSenderName() {
+    return this.message.from.first_name;
+  }
 
-    getSenderName() {
-      return this.message.from.first_name;
-    }
+  getLowerCaseTextMessage() {
+    return this.message.text.toLowerCase();
+  }
 
-    getLowerCaseTextMessage() {
-      return this.message.text.toLowerCase();
-    }
-
-    getChatId() {
-      return this.message.chat.id;
-    }
-  };
+  getChatId() {
+    return this.message.chat.id;
+  }
 };
